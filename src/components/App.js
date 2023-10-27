5c409e0e55f5bb582cd990a37066b8260ba2af15
Ashwani Kumar <130587125+ashwanimaurya85@users.noreply.github.com>
2023-10-27 07:53:48 +0000

    firat

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,55 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
 
-const App = () => {
+
+
+
+
+import axios from "axios";
+
+function App() {
+  const [users, setUsers] = useState([]);
+  const API_URL = "https://reqres.in/api/users";
+
+  const getUsers = async () => {
+    try {
+      const response = await axios.get(API_URL);
+      setUsers(response.data.data);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  };
+
   return (
     <div>
-        {/* Do not remove the main div */}
+      <button className="btn" onClick={getUsers}>
+        Get User List
+      </button>
+      <table>
+        <thead>
+          <tr>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Email</th>
+            <th>Avatar</th>
+          </tr>
+        </thead>
+        <tbody>
+          {users.map((user) => (
+            <tr key={user.id}>
+              <td>{user.first_name}</td>
+              <td>{user.last_name}</td>
+              <td>{user.email}</td>
+              <td>
+                <img src={user.avatar} alt={`${user.first_name} Avatar`} />
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
